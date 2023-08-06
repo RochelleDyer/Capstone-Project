f1c2cad865eb426839e37a1be86d4f2aaca6550a
Rochelle Dyer <rochelle.dyer@gmail.com>
2023-08-06 08:49:09 -0400

    Capstone Project

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,22 +8,26 @@ function Contact() {
           <div className='mr-60 p-10'>
             <table>
             <ul>            
-                <li><h3 className='p-5 text-white text-3xl'><u>Contact Us on:</u> </h3></li> 
+                <li><h3 className='p-3 text-white text-3xl'><u>Contact Us on:</u> </h3></li> 
                 
-                <li><i className="fa-brands fa-facebook text-lg text-white">   Facebook </i></li>             
+                <li><i className="fa-brands fa-facebook text-lg text-white">   Facebook </i>
+                <p className = 'text-white'>www.facebook.com/DMA</p></li>             
                 <br></br>                       
-                <li><i className="fa-brands fa-twitter text-lg text-white"> Twitter </i></li>                  
+                <li><i className="fa-brands fa-twitter text-lg text-white"> Twitter </i>
+                <p className = 'text-white'>www.twitter.com/DMA</p></li>                  
                 <br></br>          
-                <li><i className="fa-brands fa-instagram text-lg text-white"> Instagram </i></li> 
+                <li><i className="fa-brands fa-instagram text-lg text-white"> Instagram </i>
+                <p className = 'text-white'>www.instagram.com/DMA</p></li> 
                 <br></br>
-                <li><i className="fa-brands fa-snapchat text-lg text-white"> Snapchat </i></li>
+                <li><i className="fa-brands fa-snapchat text-lg text-white"> Snapchat </i>
+                <p className = 'text-white'>www.snapchat.com/DMA</p></li>
                 <br></br> 
                 <li> <h3 className='p-5 text-white text-3xl'><u> Or Call Us at: </u> </h3></li>
              
                 <li><i className="fa-solid fa-mobile text-lg text-white"> 555-555-5555 </i></li>
-                <br></br> 
+                 
                 <li><h2 className='text-white'> Hope to hear from you soon! </h2></li>
-                <br></br>
+            
                   
             </ul>  
             </table>
